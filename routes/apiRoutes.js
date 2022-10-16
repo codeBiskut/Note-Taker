8e1cdfeb646e7f54425bcc14c3aec01a3a054e69
codeBiskut <josh.rinehart.2k@gmail.com>
2022-10-16 13:40:22 -0400

    created route folder and files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,2 @@
+// GET /api/notes should read the db.json file and return all saved notes as JSON
+// POST /api/notes should recieve a new note to save on the request body, add i tto tthe db.json file, and then return the ndew note to the client. find a way to give each note a unique id when it's saved (look into npm packages that could do this for you )
