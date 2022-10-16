a54ed959fb17e5b3a9d34c8d5a6991529119318f
codeBiskut <josh.rinehart.2k@gmail.com>
2022-10-16 14:06:38 -0400

    added server routes for html pages

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,8 @@
+const router = require("express").Router()
+const fs = require("fs")
+const db = require("../db/db.json")
+
 // GET /api/notes should read the db.json file and return all saved notes as JSON
 // POST /api/notes should recieve a new note to save on the request body, add i tto tthe db.json file, and then return the ndew note to the client. find a way to give each note a unique id when it's saved (look into npm packages that could do this for you )
+
+router.get("/api/notes")
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("*", (req, res) => {res.sendFile(path.join(__dirname, 'index.html'))})
+app.get("/notes", (req, res) => {res.sendFile(path.join(__dirname, 'notes.html'))})
